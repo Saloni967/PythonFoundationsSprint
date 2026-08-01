60ad98f92b0fa79dd4a13c6bb50bc7f142cfdcd9
Saloni Gurav <salonigurav282@gmail.com>
2026-08-01 19:20:35 +0530

    Add files via upload

---
+++ day1_basics.py
@@ -0,0 +1,9 @@
+# Take user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Print formatted message
+print(f"Hello {name}, you are {age} years old.")
+
+# Print age after 5 years
+print(f"In 5 years, you will be {age + 5}.")
