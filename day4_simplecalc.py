743f6f3fb123afcb5deb57805566ec06696ac966
Saloni Gurav <salonigurav282@gmail.com>
2026-08-05 18:18:04 +0530

    Add files via upload

---
+++ day4_simplecalc.py
@@ -0,0 +1,23 @@
+# Simple Command-Line Calculator
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print("Result:", num1 + num2)
+
+elif operator == "-":
+    print("Result:", num1 - num2)
+
+elif operator == "*":
+    print("Result:", num1 * num2)
+
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print("Result:", num1 / num2)
+
+else:
+    print("Error: Invalid operator.")
