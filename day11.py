def1cf2f33d14366a55512cfc9ff4e263d306999
Saloni Gurav <salonigurav282@gmail.com>
2026-08-18 20:26:14 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,13 @@
+def moveZeroes(nums):
+    left = 0
+
+    for right in range(len(nums)):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+
+nums = [0, 1, 0, 3, 12]
+
+moveZeroes(nums)
+
+print(nums)
